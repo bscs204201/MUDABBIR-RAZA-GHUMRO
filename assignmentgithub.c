3143c1ca52ff92cca78380ee8df3a827d2110189
bscs204201 <74551526+bscs204201@users.noreply.github.com>
2020-11-16 21:40:55 +0500

    Add files via upload

---
+++ assignmentgithub.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+void main(void)
+{
+   float x,y,avg;
+   printf("Enter first number:\n");
+   scanf("%f",&x);
+   
+   printf("Enter second number:\n");
+   scanf("%f",&y);
+   
+   avg=(x+y)/2;
+   printf("The average is:%f\n",avg);
+
+   
+}
